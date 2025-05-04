f5a49557a49eddb59ac0ea0d16ca807f02f1419e
erienx <mat21mat12mat@gmail.com>
2025-05-04 23:22:09 +0200

    added authors endpoint

---
+++ backend/src/main/java/com/example/library/controller/AuthorController.java
@@ -0,0 +1,23 @@
+package com.example.library.controller;
+
+import com.example.library.model.Author;
+import com.example.library.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/authors")
+public class AuthorController {
+
+    @Autowired
+    private AuthorService authorService;
+
+    @GetMapping
+    public List<Author> getAllAuthors(){
+        return authorService.findAllAuthors();
+    }
+}
--- backend/src/main/java/com/example/library/service/AuthorService.java
+++ backend/src/main/java/com/example/library/service/AuthorService.java
@@ -1,14 +1,21 @@
 package com.example.library.service;
 
 import com.example.library.model.Author;
+import com.example.library.model.Category;
 import com.example.library.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthorService {
     @Autowired
     private AuthorRepository authorRepository;
+
+    public List<Author> findAllAuthors(){
+        return authorRepository.findAll();
+    }
 
     public Author getOrAddAuthor(String authorName){
         return authorRepository.findByAuthorName(authorName)
